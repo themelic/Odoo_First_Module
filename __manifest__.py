bf273eaf2eba1bab0c54eda7be9c5d6a666330fc
Melih Melik SÖNMEZ <5006654+themelic@users.noreply.github.com>
2026-06-05 01:48:13 +0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'My First Module',
+    'version': '17.0.1.0.0',
+    'category': 'Custom',
+    'author': 'MMS ',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/my_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
